0bf2b4dd0dba8682e0411b8a104a20ef896ca131
Rani <ranipardhi936@gmail.com>
2023-12-25 00:04:35 +0530

    first commit

---
+++ socketapi.js
@@ -0,0 +1,24 @@
+const io = require( "socket.io" )();
+const socketapi = {
+    io: io
+};
+
+// Add your socket.io logic here!
+io.on( "connection", function( socket ) {
+    console.log( "A user connected" );
+
+    socket.on('sony', msg=>{
+        console.log(msg);
+
+        socket.broadcast.to(msg.roomName).emit('max', msg);
+    })
+
+    socket.on('joinRoom', roomName=>{
+        socket.join(roomName);
+
+    })
+
+});
+// end of socket.io logic
+
+module.exports = socketapi;
